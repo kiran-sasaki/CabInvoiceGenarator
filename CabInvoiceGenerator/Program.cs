8114187c463e197a12d9cf9146060d2cceb8c7a3
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-10 13:07:58 +0530

    [Kiran] Add: Invoice Test Case

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace CabinvoiceGenerator
+﻿namespace CabInvoiceGenerator
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Cab Invoice Generator");
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoice.CalculateFare(2.0, 5);
+            Console.WriteLine($"FARE : {fare}");
         }
     }
 }
---
+++ CabInvoiceTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+using CabInvoiceGenerator;
+
+namespace CabInvoiceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public InvoiceGenerator invoiceGenerator = null;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+        [TestMethod]
+        public void GivenMultipleRideShouldReturnInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1,1) };
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary=new InvoiceSummary(2, 30.0);
+            Assert.AreEqual(expectedSummary,summary);
+        }
+    }
+}
