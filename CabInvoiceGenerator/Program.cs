a418cb64e70bdee2f019c5cc8d32454cb30438ae
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-10 11:44:41 +0530

    [Kiran] Add: Welcome Messsage

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CabinvoiceGenerator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cab Invoice Generator");
+        }
+    }
+}
